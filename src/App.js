9c48359d0aa1c68150606af4ae9f530148b30817
robertmtpaul <rob@glo.id.au>
2020-09-19 13:59:19 +1000

    Working sidebar, product details and images showing, stylesheet initialised. Installed react-router-dom. Added router to document; added seed data to data.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data'
+import { Route, Link, HashRouter as Router } from 'react-router-dom';
 
 function App() {
+
+  // Open the sidebar with categories with vanillaJS DOM manip.
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  // close the sidebar with categories.
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+                </button>
+            <a href="index.html">Home</a>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign in </a>
+          </div>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+              <li>
+                <a href="index.html">Electronics</a>
+              </li>
+              <li>
+                <a href="index.html">Clothes</a>
+              </li>
+            </ul>
+          </aside>
+
+        </header>
+        <aside>
+          Shopping Categories
+        </aside>
+        <main className="main">
+          <div className="content">
+            <ul className="products">
+              {
+                data.products.map(product =>
+                  <li>
+                    <div className="product">
+                      <img className="product-image" src={product.image} alt={product.name} />
+                      <div className="product-name">
+                        <a href="product.html">{product.name}</a>
+                      </div>
+                      <div className="product-brand">{product.brand}</div>
+                      <div className="product-price">${product.price}</div>
+                      <div className="product-rating">{product.rating} Stars({product.numReviews} Reviews)</div>
+                    </div>
+                  </li>)
+              }
+            </ul>
+          </div>
+        </main>
+        <footer className="footer">
+          All rights reserved.
+        </footer>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            name: 'PS5',
+            category: 'Electronics',
+            image: 'https://source.unsplash.com/random/200x300',
+            price: 200,
+            brand: 'Sony',
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            name: 'Nice Suitcase',
+            category: 'Clothes',
+            image: 'https://source.unsplash.com/random/300x300',
+            price: 200,
+            brand: 'Samsonite',
+            rating: 4.5,
+            numReviews: 5
+        },
+        {
+            name: 'Xbox',
+            category: 'Electronics',
+            image: 'https://source.unsplash.com/random/200x300',
+            price: 200,
+            brand: 'Microsoft',
+            rating: 2.2,
+            numReviews: 2
+        },
+        {
+            name: 'Wii',
+            category: 'Travel',
+            image: 'https://source.unsplash.com/random/200x300',
+            price: 200,
+            brand: 'Nintendo',
+            rating: 2.2,
+            numReviews: 3
+        },
+    ]
+}
