7acca70ede02044104a86d418ac2a50d528da654
robertmtpaul <rob@glo.id.au>
2020-10-10 12:02:58 +1100

    Refactor of App.js to remove props being passed into product details page.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Route, HashRouter as Router } from 'react-router-dom';
 // import website components
 import ProductIndex from './components/ProductIndex'
 import Header from './components/Header'
-import Cart from './components/Cart'
 import ProductDetails from './components/ProductDetails'
 import Checkout from './components/Checkout'
 import Login from './components/Login'
@@ -18,36 +17,38 @@ class App extends React.Component {
     cart: [],
     cartCount: 0,
   }
-  
-  addToCart = (product, qty) => {
-    // console.log('in add to cart: ', product, qty )
-    
-    // Set a variable that keeps an updated version of the cart i.e. 
-    ///'...'retains what is already in cart
-    const newCart = [ ...this.state.cart, {product, qty } ];
-    // console.log('quantities' , this.state.cartCount, qty)
-    // Put what is in the cart in state. 
-    this.setState({ cart: newCart, cartCount: this.state.cartCount + qty })
-    localStorage.setItem('cart', JSON.stringify(newCart))
-  }
 
-  removeItem = (removeId) => {// Here the Id of the item to be removed is passed in     
-        console.log(removeId);
-    // Here we make a copy of the cart's current state
-        const cartCopy = [...this.state.cart];
-    // Loop through the cart's current state, passing in the product to be deleted and the index of the item 
-        this.state.cart.forEach( (cartProduct, index) => {
-            console.log('product ebeing compared', cartProduct);
-    // We compare the IDs of the cart products array and check for a match of the product ID being passed in
-            if (cartProduct.product._id === removeId) {
-                console.log('removed', cartProduct);
-    // We then use splice to first get the index of that product in the cart array and delete it from the caopy of the cart
-                cartCopy.splice(index, 1);
-            }
-        });
-    // And now the updated version of the copy we made of the cart is put into state
-        this.setState({cart: cartCopy})
-    }
+  // PREVIOUS CODE:
+  // import Cart from './components/Cart'
+  // addToCart = (product, qty) => {
+  //   // console.log('in add to cart: ', product, qty )
+
+  //   // Set a variable that keeps an updated version of the cart i.e. 
+  //   ///'...'retains what is already in cart
+  //   const newCart = [ ...this.state.cart, {product, qty } ];
+  //   // console.log('quantities' , this.state.cartCount, qty)
+  //   // Put what is in the cart in state. 
+  //   this.setState({ cart: newCart, cartCount: this.state.cartCount + qty })
+  //   localStorage.setItem('cart', JSON.stringify(newCart))
+  // }
+
+  // removeItem = (removeId) => {// Here the Id of the item to be removed is passed in     
+  //       console.log(removeId);
+  //   // Here we make a copy of the cart's current state
+  //       const cartCopy = [...this.state.cart];
+  //   // Loop through the cart's current state, passing in the product to be deleted and the index of the item 
+  //       this.state.cart.forEach( (cartProduct, index) => {
+  //           console.log('product ebeing compared', cartProduct);
+  //   // We compare the IDs of the cart products array and check for a match of the product ID being passed in
+  //           if (cartProduct.product._id === removeId) {
+  //               console.log('removed', cartProduct);
+  //   // We then use splice to first get the index of that product in the cart array and delete it from the caopy of the cart
+  //               cartCopy.splice(index, 1);
+  //           }
+  //       });
+  //   // And now the updated version of the copy we made of the cart is put into state
+  //       this.setState({cart: cartCopy})
+  //   }
 
 
   componentDidMount() {
@@ -57,17 +58,17 @@ class App extends React.Component {
     // And if not null, set token into the axios header
     // which will cause axios headers to be authenticated
     // set user object into state.
-    if( token !== null && user !== null) {
-      this.setState({ user: JSON.parse(user)} );
+    if (token !== null && user !== null) {
+      this.setState({ user: JSON.parse(user) });
       axios.defaults.headers.common.Authorization = `Bearer ${token}`;
     }
     // Load shopping cart from local storage if available
     // if not null, assume ... (?)
-    const cart = localStorage.getItem( 'cart')
+    const cart = localStorage.getItem('cart')
     if (cart !== null) {
       // JSON.parse gets the string back and turns into array of objects/
-      this.setState({cart: JSON.parse(cart)})
-    } 
+      this.setState({ cart: JSON.parse(cart) })
+    }
   }
 
   // passes as arguments token and user that backend sent
@@ -78,50 +79,42 @@ class App extends React.Component {
     // store the current user into state as an object.
     this.setState({ user })
     // save the token into local storage for use by App.js and other components on site.
-    localStorage.setItem( 'token', token)
+    localStorage.setItem('token', token)
     // local storage can only set strings, can't set complex items, so needs to have the JSON stringified.
-    localStorage.setItem( 'user', JSON.stringify(user) )
+    localStorage.setItem('user', JSON.stringify(user))
   }
 
-    performLogout = ( event )=> {
-      event.preventDefault()
-      delete axios.defaults.headers.common.Authorization
+  performLogout = (event) => {
+    event.preventDefault()
+    delete axios.defaults.headers.common.Authorization
 
-      // Remove the cart & users props stored in local storage.
-      localStorage.removeItem("token")
-      localStorage.removeItem("user")
-      // set the user state as empty object
-      this.setState({ user: {} })
-    } 
+    // Remove the cart & users props stored in local storage.
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    // set the user state as empty object
+    this.setState({ user: {} })
+  }
 
   render() {
     return (
-      
+
       <Router>
         <div>
-          <Route path="/" render={(props) => <Header {...props} onLogout={this.performLogout} currentUser={this.state.user} cartCount={this.state.cartCount}/>  } />
+          <Route path="/" render={(props) => <Header {...props} onLogout={this.performLogout} currentUser={this.state.user} cartCount={this.state.cartCount} />} />
           {/*        
             Pass in ROUTER PROPS into the 
           */}
-          <Route exact path="/login" render={(props) => <Login {...props} onLogin={this.performLogin} />  } />
+          <Route exact path="/login" render={(props) => <Login {...props} onLogin={this.performLogin} />} />
           <main className="main">
-          <div>
-           
-            <br/> 
-          </div>
+            <div>
+
+              <br />
+            </div>
             <div className="content">
               <Route exact path="/" component={ProductIndex} />
-              
-              <Route exact path="/products/:id" render={(props) => 
-                <ProductDetails 
-                {...props} 
-                onAddToCart={this.addToCart} 
-                cart={this.state.cart}
-                />  
-              } 
-              />
-          <Route path="/cart" /> 
-          <Route path="/checkout" component={Checkout}/> 
+              <Route exact path="/products/:id" component={ProductDetails} />
+              <Route path="/cart" />
+              <Route path="/checkout" component={Checkout} />
             </div>
           </main>
           <footer className="footer">
--- src/components/ProductIndex.js
+++ src/components/ProductIndex.js
@@ -2,35 +2,62 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios';
-import {PRODUCTS_URL} from '../constants'
+import { PRODUCTS_URL } from '../constants'
 import '../Products.css'
 import { useStateValue } from "../StateProvider";
 
-function ProductIndex (props) {
-    // put the product and setProduct into useState
-    const [ products, setProducts ] = useState ([]);   
-    // getting cart, and the dispatch. 
-    const [{cart}, dispatch] = useStateValue(); 
+function ProductIndex(props) {
+  // put the product and setProduct into useState
+  const [products, setProducts] = useState([]);
+  // getting cart, and the dispatch. 
+  const [{ cart }, dispatch] = useStateValue();
 
-    useEffect(() => {
-      const fetchProducts = async () => {
-        // use axios to retrieve products from backend API and store as variable 'data'
-        const { data } = await axios.get(PRODUCTS_URL)
-        // Set the data retrieved via axios request into state in setProduct
-        setProducts(data);
-      }
-      fetchProducts(); 
-      return () => {
-        // cleanup
-      }
-    }, [])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // use axios to retrieve products from backend API and store as variable 'data'
+      const { data } = await axios.get(PRODUCTS_URL)
+      // Set the data retrieved via axios request into state in setProduct
+      console.log(data);
+      setProducts(data);
+    }
+    fetchProducts();
+    return () => {
+      // cleanup
+    }
+  }, [])
 
-    return <ul className="products">
+  const addToCart = (product) => {
+    // Add item to cart...
+    const payload = {
+      type: 'ADD_TO_CART',
+      product: { // passing a payload.
+        product: product._id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        rating: product.rating
+      }
+    };
+    console.log(payload);
+    // dispatch()
+    dispatch({
+      type: 'ADD_TO_CART',
+      item: {
+        product: product._id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        rating: product.rating
+      }
+    })
+  };
+
+  return <ul className="products">
     {
       // Map through products in state, 
       products.map(product =>
         //set key to something unique like productID to satisfy map function
-        <li key = { product._id }>
+        <li key={product._id}>
           <div className="product">
             <Link to={`${/products/}${product._id}`}>
               <img className="product-image" src={product.image} alt={product.name} />
@@ -41,11 +68,12 @@ function ProductIndex (props) {
             <div className="product-brand">{product.brand}</div>
             <div className="product-price">${product.price}</div>
             <div className="product-rating">{product.rating} Stars({product.numReviews} Reviews)</div>
+            <button onClick={() => addToCart(product)}>Add to cart</button>
           </div>
         </li>)
     }
   </ul>
-    
+
 }
 
 export default ProductIndex;
