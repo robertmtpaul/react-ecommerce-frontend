a4bda2927221a2967787814b40356342d55bee6a
robertmtpaul <rob@glo.id.au>
2021-05-06 08:38:32 +0800

    Added code and icons for custom favicon

--- build/precache-manifest.f3479e3070383998d86cd7f897eb7607.js
+++ build/precache-manifest.f3479e3070383998d86cd7f897eb7607.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bdf7b357ce1ef2da79d97fba10af9ed",
+    "revision": "913149fd1ac8ee85ed08981c25cfc17a",
     "url": "/react-ecommerce-frontend/index.html"
   },
   {
-    "revision": "2d22fad19c028c9a7079",
+    "revision": "3be6621b71d556138b72",
     "url": "/react-ecommerce-frontend/static/css/main.20d1ac15.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-ecommerce-frontend/static/js/2.bb521e1a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d22fad19c028c9a7079",
-    "url": "/react-ecommerce-frontend/static/js/main.1239ccee.chunk.js"
+    "revision": "3be6621b71d556138b72",
+    "url": "/react-ecommerce-frontend/static/js/main.45412c43.chunk.js"
   },
   {
     "revision": "5c53ed31fc30e622618f",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ecommerce-frontend/precache-manifest.1d6acb4692b67acdf0f21cb350f0189d.js"
+  "/react-ecommerce-frontend/precache-manifest.f3479e3070383998d86cd7f897eb7607.js"
 );
 
 self.addEventListener('message', (event) => {
